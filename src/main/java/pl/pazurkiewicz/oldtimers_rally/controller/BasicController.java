292b38f9e4c989dd6fdd259e19ad4b7b9bebfa64
Artur Pazurkiewicz <pazurkiewiczartur@gmail.com>
2022-01-02 13:54:54 +0100

    done

--- src/main/java/pl/pazurkiewicz/oldtimers_rally/controller/BasicController.java
+++ src/main/java/pl/pazurkiewicz/oldtimers_rally/controller/BasicController.java
@@ -8,28 +8,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.pazurkiewicz.oldtimers_rally.model.Crew;
 import pl.pazurkiewicz.oldtimers_rally.model.Event;
 import pl.pazurkiewicz.oldtimers_rally.model.Language;
+import pl.pazurkiewicz.oldtimers_rally.model.QrCode;
 import pl.pazurkiewicz.oldtimers_rally.repositiory.EventRepository;
 import pl.pazurkiewicz.oldtimers_rally.repositiory.LanguageRepository;
+import pl.pazurkiewicz.oldtimers_rally.repositiory.QrCodeRepository;
 import pl.pazurkiewicz.oldtimers_rally.security.service.UserDetailsImpl;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping
 public class BasicController {
     private final EventRepository eventRepository;
     private final LanguageRepository languageRepository;
+    private final QrCodeRepository qrCodeRepository;
 
 
-    public BasicController(EventRepository eventRepository, LanguageRepository languageRepository) {
+    public BasicController(EventRepository eventRepository, LanguageRepository languageRepository, QrCodeRepository qrCodeRepository) {
         this.eventRepository = eventRepository;
         this.languageRepository = languageRepository;
+        this.qrCodeRepository = qrCodeRepository;
     }
 
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
-    String getIndex(Model model, @AuthenticationPrincipal UserDetailsImpl principal, @RequestParam(name = "404", defaultValue = "false", required = false) boolean occurred404) {
+    String getIndex(@RequestParam(name = "qr", required = false) String qr, RedirectAttributes redirectAttributes, Model model, @AuthenticationPrincipal UserDetailsImpl principal, @RequestParam(name = "404", defaultValue = "false", required = false) boolean occurred404) {
+        if (qr != null) {
+            qr = '%' + qr;
+            Optional<QrCode> qrCode = qrCodeRepository.findByQrLike(qr);
+            if (qrCode.isPresent()) {
+                Crew crew = qrCode.get().getCrew();
+                redirectAttributes.addAttribute("url", crew.getEvent().getUrl());
+                redirectAttributes.addAttribute("crewId", crew.getId());
+                return "redirect:/{url}/crew/{crewId}";
+            }
+        }
         if (principal != null) {
             List<?> events = eventRepository.getEventsWithGlobalJudgePrivilegesForId(principal.getUser().getId(), Event.class);
             if (events.isEmpty()) {
--- src/main/java/pl/pazurkiewicz/oldtimers_rally/repositiory/QrCodeRepository.java
+++ src/main/java/pl/pazurkiewicz/oldtimers_rally/repositiory/QrCodeRepository.java
@@ -1,13 +1,18 @@
 package pl.pazurkiewicz.oldtimers_rally.repositiory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.pazurkiewicz.oldtimers_rally.model.Event;
 import pl.pazurkiewicz.oldtimers_rally.model.QrCode;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface QrCodeRepository extends JpaRepository<QrCode, Integer> {
     Set<QrCode> getAllByEvent(Event event);
 
     Set<QrCode> getByEventAndCrewIsNull(Event event);
+
+    @Query("select q from QrCode q where q.qr like :qr")
+    Optional<QrCode> findByQrLike(String qr);
 }
