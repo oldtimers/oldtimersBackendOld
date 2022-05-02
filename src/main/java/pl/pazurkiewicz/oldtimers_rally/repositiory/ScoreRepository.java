0dd037d94e2eb4617fff8dfbb5ee2b7adb27b08a
Artur Pazurkiewicz <pazurkiewicz.artur@gmail.com>
2022-05-02 22:49:47 +0200

    working on invalid scores

--- src/main/java/pl/pazurkiewicz/oldtimers_rally/repositiory/ScoreRepository.java
+++ src/main/java/pl/pazurkiewicz/oldtimers_rally/repositiory/ScoreRepository.java
@@ -1,15 +1,24 @@
 package pl.pazurkiewicz.oldtimers_rally.repositiory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.pazurkiewicz.oldtimers_rally.model.Competition;
 import pl.pazurkiewicz.oldtimers_rally.model.Crew;
+import pl.pazurkiewicz.oldtimers_rally.model.Event;
 import pl.pazurkiewicz.oldtimers_rally.model.Score;
 
 import java.util.List;
 import java.util.Set;
 
 public interface ScoreRepository extends JpaRepository<Score, Integer> {
-    List<Score> getByCompetitionAndResultNotNullAndCrew_InOrderByResult(Competition competition, Set<Crew> crews);
+    @Query("select s from Score s where s.competition=:competition and s.crew in :crews and s.result is not null and s.invalidResult = false order by s.result")
+    List<Score> getValidScoresForCompetition(Competition competition, Set<Crew> crews);
+
+    @Query("select s from Score s where s.competition=:competition and s.crew in :crews and s.result is not null and s.invalidResult = true order by s.result")
+    List<Score> getInvalidScoresForCompetition(Competition competition, Set<Crew> crews);
+
+    @Query("select s.competition.id,s.crew.id from Score s where s.competition.event=:event group by s.competition, s.crew having count(s.crew)>1 order by s.competition.id, s.crew.id")
+    List<List<Integer>> getDuplicates(Event event);
 
     Score getByCompetitionAndCrew(Competition competition, Crew crew);
 }
